bc569e114885c8dbba7540f075908f27882c0359
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-02 10:00:31 +0200

    message works

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@ import { useState } from 'react';
 import './App.scss'
 
 function App() {
-	const [message, setMessage] = useState('nnn2222');
+	const [message, setMessage] = useState('');
 	return (
 		<div className="App">
 		<h1>Zustand Example</h1>
+
+			<main>
+				<section className="controlArea">
+
+					<div className="data">
+					<label>Message:</label>
+						<input type="text"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+					</div>
+
+				</section>
+
+			</main>
 			<p>message: {message}</p>
 		</div>
 	)
